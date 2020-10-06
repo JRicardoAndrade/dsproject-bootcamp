14b431778f306a510fda6238693ffbff9098f7fb
JRicardoAndrade <jricardoandradecosta@gmail.com>
2020-10-06 16:10:30 -0300

    Database seeding

--- backend/src/main/java/com/ricardo/dsproject/entities/Client.java
+++ backend/src/main/java/com/ricardo/dsproject/entities/Client.java
@@ -3,6 +3,7 @@ package com.ricardo.dsproject.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -26,6 +27,8 @@ public class Client implements Serializable{
 	private String name;
 	private String cpf;
 	private Double income;
+	
+	@Column (columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant birthDate;
 	private Integer children; 
 
